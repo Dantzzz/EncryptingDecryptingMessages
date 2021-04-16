e4e21ac5868a1bb17da3c12ee940c01b2b75aca0
Dantz Farrow <dantzfarrow@gmail.com>
2021-04-15 21:12:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EncryptingDecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plain_text = Util.GetPlainText();
+            string single_key = Util.GetSingleKey();
+            string multi_key = Util.GetMultiKey();
+            Console.WriteLine();
+
+            Console.WriteLine( $"You entered [{plain_text}] as plain text. ");
+            Console.WriteLine( $"You entered [{single_key}] as your single key. ");
+            Console.WriteLine( $"You entered [{multi_key}] as your multi key. ");
+            Console.WriteLine();
+
+            int[] clean_text = Util.Clean(plain_text);
+            int[] clean_skey = Util.Clean(single_key);
+            int[] clean_mkey = Util.Clean(multi_key);
+
+            string enc_single = Util.SingleEnc(clean_text, clean_skey);
+            string enc_multi = Util.MultiEnc(clean_text, clean_mkey);
+            string enc_conti = Util.ContiEnc(clean_text, clean_mkey);
+            
+            Console.WriteLine( $"Encrypted message with single_key is [{enc_single}]. ");
+            Console.WriteLine( $"Encrypted message with multi_key is [{enc_multi}]. ");
+            Console.WriteLine( $"Encrypted message with cont inuous key i s [{enc_conti }]. ");
+            Console.WriteLine();
+
+            string dec_single = Util.SingleDec( enc_single, clean_skey);
+            string dec_multi = Util.MultiDec(enc_multi, clean_mkey);
+            string dec_conti = Util.ContiDec(enc_conti, clean_mkey);
+
+            Console.WriteLine( $"Decrypted message with single_key is [{dec_single}] ");
+            Console.WriteLine( $"Decrypted message with multi_key is [{dec_multi}]. ");
+            Console.WriteLine( $"Decrypted message with continuous_key is [{dec_conti}] ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptingDecryptingMessages
+{
+    class Util
+    {
+        public static string GetPlainText()
+        {
+            Console.WriteLine("Enter plain text: ");
+            return Console.ReadLine();
+        }
+
+        public static string GetSingleKey()
+        {
+            try 
+            {
+                Console.WriteLine("Enter a value for single key: ");
+                char single_keyEntry = Char.Parse(Console.ReadLine());
+                return single_keyEntry.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Try again... Please enter one character: ");
+                GetSingleKey();
+                return null;
+            }
+        }
+
+        public static string GetMultiKey()
+        {
+            Console.WriteLine("Enter a value for multi key: ");
+            string multi_keyEntry = Console.ReadLine();
+            return multi_keyEntry;
+        }
+
+        public static int[] Clean(string plain_text)
+        {
+            string upperPlainText = plain_text.ToUpper();
+            char[] singleCharacters = upperPlainText.ToCharArray();
+            List<int> asciiCharacters = new List<int>();
+
+            for (int i = 0; i < singleCharacters.Length; i++)
+            {
+                int toCheck = (int)singleCharacters[i];
+                if (toCheck >= 65 && toCheck <= 90) 
+                {
+                    asciiCharacters.Add(toCheck);
+                }
+            }
+
+            return asciiCharacters.ToArray();
+        }
+        
+        public static string SingleEnc(int[] clean_text, int[] clean_skey)
+        {
+            int movePlaces = clean_skey[0] - 64;
+            string encryptedString = "";
+
+            for (int i = 0; i < clean_text.Length; i++)
+            {
+                int newASCIILocation = clean_text[i] + movePlaces;
+                encryptedString = newASCIILocation.ToString();
+            }
+            return encryptedString;
+        }
+        
+        public static string MultiEnc(int[] clean_text, int[] clean_mkey)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static string ContiEnc(int[] clean_text, int[] clean_mkey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string SingleDec(string enc_single, int[] clean_skey)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static string MultiDec(string enc_multi, int[] clean_mkey)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static string ContiDec(string enc_conti, int[] clean_mkey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
